2173ca1775870639f0b02c8e029dcb4d0e52b46a
guzman.bossi <guzman.bossi@gmail.com>
2024-07-31 23:37:46 +0200

    wip

--- BlogServer/BossiIt.BlogServer/Services/Implementation/BlogPostService.cs
+++ BlogServer/BossiIt.BlogServer/Services/Implementation/BlogPostService.cs
@@ -1,6 +1,10 @@
-﻿namespace BossiIt.BlogServer.Services.Implementation
+﻿using BossiIt.BlogServer.Domain.Models;
+
+namespace BossiIt.BlogServer.Services.Implementation
 {
     public class BlogPostService : IBlogPostService
     {
+
+
     }
 }
